5db0f4544ab97de3da6ed18feb501082e2f5dfce
LuisMiguel <luismiguel110205@gmail.com>
2024-08-21 12:59:35 -0500

    feat :sparkles: creation day 3

---
+++ Dia_3/script.js
@@ -0,0 +1,28 @@
+
+var numeron = prompt("numero");
+var pru;
+var numeroy;
+var numerox=0;
+var bool=true;
+while (bool==true) {
+    pru = numeron.length;
+    console.log(pru);
+    
+    if (pru>1){
+        for (const i of numeron){
+            numerox+=(Math.floor(i));
+            
+            console.log(numerox);
+            
+        }
+        numeroy =numerox.toString()
+        pru = numeroy.length;
+        console.log(pru);
+        
+    }
+    else if (pru==1){
+        console.log(numerox);
+        bool=false;
+    }
+    numeron=numerox;
+}
