f66a00399bb90f48466d0ff65362130658aab7be
luis miguel <luismiguelcaicedobermon@gmail.com>
2024-09-02 15:01:00 -0500

    feat :sparkles: added functionality to 'prev and next' buttons

--- Dia_9/script.js
+++ Dia_9/script.js
@@ -6,6 +6,7 @@ function obtener_id(){
     let id_nombre = document.getElementById("id_nombre").value;
     buscar(id_nombre)
 }
+let id;
 function buscar(id_nombre){
     let link = "https://pokeapi.co/api/v2/pokemon/"+id_nombre;
     fetch(link)
@@ -15,5 +16,47 @@ function buscar(id_nombre){
         document.getElementById("gif").innerHTML += `
         <img src="${info.sprites.other.showdown.front_default}">
         `
+        document.getElementById("nombre").innerHTML=""
+        document.getElementById("nombre").innerHTML+=`
+        ${info.id} - 
+        <p id="nom"> ${info.forms[0].name}</p>
+        `
+        id = info.id
+    })
+}
+function next(){
+    let nuevo_id = id+1
+    let link = "https://pokeapi.co/api/v2/pokemon/"+nuevo_id;
+    fetch(link)
+    .then(res => res.json())
+    .then(info =>{
+        document.getElementById("gif").innerHTML = ""
+        document.getElementById("gif").innerHTML += `
+        <img src="${info.sprites.other.showdown.front_default}">
+        `
+        document.getElementById("nombre").innerHTML=""
+        document.getElementById("nombre").innerHTML+=`
+        ${info.id} - 
+        <p id="nom"> ${info.forms[0].name}</p>
+        `
+        id = info.id
+    })
+}
+function prev(){
+    let nuevo_id = id-1
+    let link = "https://pokeapi.co/api/v2/pokemon/"+nuevo_id;
+    fetch(link)
+    .then(res => res.json())
+    .then(info =>{
+        document.getElementById("gif").innerHTML = ""
+        document.getElementById("gif").innerHTML += `
+        <img src="${info.sprites.other.showdown.front_default}">
+        `
+        document.getElementById("nombre").innerHTML=""
+        document.getElementById("nombre").innerHTML+=`
+        ${info.id} - 
+        <p id="nom"> ${info.forms[0].name}</p>
+        `
+        id = info.id
     })
 }
