ace1a17ed7a50b543d8bdcb4758a16e536b10600
luismiguel <luismiguel110205@gmail.com>
2024-09-06 12:58:05 -0500

    feat: :sparkles: day 12

---
+++ Dia_12/js/script.js
@@ -0,0 +1,58 @@
+document.addEventListener("DOMContentLoaded",() => {
+    const datosContenedor = document.querySelector(".opciones");
+    const taskInput = document.querySelector(".taskInput");
+    const addTaskButton = document.querySelector(".addTaskButton");
+
+    async function fechData() {
+        const res = await fetch("https://6674179975872d0e0a950e53.mockapi.io/todoList");
+        data = await res.json();
+        return data;
+    }
+
+    function displayCapsula(capsula){
+        datosContenedor.innerHTML='';
+        capsula.forEach(cap => {
+            const capDiv = document.createElement('div')
+            if (cap.status === "ready"){
+                capDiv.classList.add('capsulanegativa')
+                capDiv.innerHTML=`
+                <div class="capsulanegativa">
+                    <div class="infotexnegativo">
+                        <p>${cap.task}</p>
+                    </div>
+                    <div class="botonesnegativo">
+                        <div class="terminadonegativo">
+                            <img src="./storage/img/pngwing.com (2).png" data-id="${cap.id}" class="completado">
+                        </div>
+                        <div class="eliminadonegativo">
+                            <img src="./storage/img/pngwing.com (4).png" data-id="${cap.id}" class="eliminado">
+                        </div>
+                    </div>
+                </div>
+                `
+            }
+            if (cap.status === "On hold") {
+                capDiv.classList.add('capsula');
+                capDiv.innerHTML = `
+                <div class="capsula">
+                    <div class="infotex">
+                        <p>${cap.task}</p>
+                    </div>
+                    <div class="botones">
+                        <div class="terminado">
+                            <img src="./storage/img/pngwing.com (2).png" data-id="${cap.id}" class="completado">
+                        </div>
+                        <div class="eliminado">
+                            <img src="./storage/img/pngwing.com (4).png" data-id="${cap.id}" class="eliminado">
+                        </div>
+                    </div>
+                </div>
+                `
+            }
+        datosContenedor.appendChild(capDiv)
+        });
+    }
+    fechData().then(data =>{
+        displayCapsula(data);
+    })
+})
