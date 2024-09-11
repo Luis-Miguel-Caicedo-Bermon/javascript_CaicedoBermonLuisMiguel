847fa2aecb96866ffcb53fed3f6623a6c873f8f2
luis miguel <luismiguelcaicedobermon@gmail.com>
2024-09-10 20:12:05 -0500

    feat: :sparkles: end of the guessing game

--- Dia_13/script.js
+++ Dia_13/script.js
@@ -9,20 +9,51 @@ fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=9")
     document.getElementById('carta_random').innerHTML=`
     <img src="${info.cards[aleatorio].image}" alt="">
     `
+    mostrar(info,aleatorio)
 });
-function mostrar(){
-    const pre = document.getElementsByClassName("carta");
-    let contador = 0
-    for (const i of pre){
-        if (i.classList.contains("mostrada")) {
-            i.classList=("carta")
+let contador = 0;
+let ganado ="";
+function mostrar(cartas,aleatorio){
+    if (contador<2) {
+        const pre = document.getElementsByClassName("carta");
+        for (let i=0;i<pre.length;i++){
+            if (pre[i].classList.contains("mostrada")) {
+                pre[i].classList=("carta")
+            }
+            pre[i].addEventListener('click',()=>{
+                if (contador<2) {
+                    pre[i].classList.add("mostrada");
+                    document.querySelector('.mostrada').innerHTML=`
+                    <img src="${cartas["cards"][i]["image"]}" alt="">
+                    `
+                    contador+=1;
+                    if (cartas["cards"][i]["image"]==cartas["cards"][aleatorio]["image"]) {
+                        document.querySelector('.contenedor').innerHTML+=`
+                        <div id="ganaste">
+                            <div>
+                                <img src="./images/ganar.png" alt="">
+                            </div>
+                            <form action="">
+                                <button>jugar de nuevo</button>
+                            </form>
+                        </div>
+                        `
+                        ganado='ganado'
+                    }
+                }
+                else if(contador>=2 && ganado!='ganado'){
+                    document.querySelector('.contenedor').innerHTML+=`
+                    <div id="ganaste">
+                        <div>
+                            <img src="./images/perder.png" alt="">
+                        </div>
+                        <form action="">
+                            <button>jugar de nuevo</button>
+                        </form>
+                    </div>
+                    `
+                }
+            })
         }
-        i.addEventListener("click",()=>{
-            i.classList.add("mostrada");
-            document.querySelector('.mostrada').innerHTML=`
-            <img src="${cartas[0]["cards"][contador]["image"]}" alt="">
-            `
-        })
-        contador+=1;
     }
 }
